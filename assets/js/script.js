a1ef8570b95af80990ccba3160dcdb727e48b962
seniorweb <seniorweb0417@outlook.com>
2022-05-27 19:17:39 +0200

    any price, any types dropdown finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,27 @@ $(document).ready(function() {
         $(this).toggleClass('on');
         $('.filter-wrapper').toggleClass('on');
     });
+
+    $('#price-slider').slider({
+        orientation: "horizontal",
+        range: "min",
+        min: 10,
+        max: 1000,
+        value: 100,
+        slide: refreshSwatch,
+        change: refreshSwatch
+    });
+
+    $('.filter-wrapper .dropdown .dropdown-menu').on('click', function (e) {
+        e.stopPropagation();
+        // $(this).parent().toggleClass('open');
+    });
 });
+
+function refreshSwatch() {
+    var slider_value = $('#price-slider').slider('value');
+    $("#slider-price").html(slider_value);
+}
 
 function showSignupModal() {
     $('#signup-modal').modal();
