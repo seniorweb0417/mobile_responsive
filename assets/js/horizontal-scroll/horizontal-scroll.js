1b16d324faa36cb7ca47259dd8a6a4e400e84661
seniorweb <seniorweb0417@outlook.com>
2022-05-30 03:40:16 +0200

    fix feedback

--- assets/js/horizontal-scroll/horizontal-scroll.js
+++ assets/js/horizontal-scroll/horizontal-scroll.js
@@ -1,14 +1,24 @@
+var parent_width = $('.list-container').width(),
+$item = $('.list'), //Cache your DOM selector
+visible = 200, //Set the number of items that will be visible
+index = 0, //Starting index
+endIndex = Math.round( ($item.width() - parent_width) / visible + 0.5 );// - 1; //End index
+
+var resizeId;
+
 $(document).ready(function() {
-    var parent_width = $('.list-container').width(),
-        $item = $('.list'), //Cache your DOM selector
-        visible = 200, //Set the number of items that will be visible
-        index = 0, //Starting index
-        endIndex = Math.round( ($item.width() - parent_width) / visible + 0.5 );// - 1; //End index
-    
-    console.log('end index:' + endIndex);
+    initialHorScroll();
+
+    $(window).on('resize', function() {
+        clearTimeout(resizeId);
+        resizeId = setTimeout(initialHorScroll, 500);
+    });
+
+    // $(window).resize(function() {
+    //     initialHorScroll();
+    // });
 
     $('.more-link-right').click(function(){
-        // $item.animate({'left':'-=' + visible + 'px'});
         if(index < endIndex ){
           index++;
           $item.animate({'left':'-=' + visible + 'px'});
@@ -16,11 +26,22 @@ $(document).ready(function() {
     });
     
     $('.more-link-left').click(function(){
-        // $item.animate({'left':'+=' + visible + 'px'});
         if(index > 0){
           index--;            
           $item.animate({'left':'+=' + visible + 'px'});
         }
     });
-    
+
 });
+
+function initialHorScroll() {
+    console.log('initialHorScroll is called');
+    
+    parent_width = $('.list-container').width(),
+    $item = $('.list'), //Cache your DOM selector
+    visible = 200, //Set the number of items that will be visible
+    index = 0, //Starting index
+    endIndex = Math.round( ($item.width() - parent_width) / visible + 0.5 );// - 1; //End index
+
+    $item.animate({'left':'0px'});
+}
